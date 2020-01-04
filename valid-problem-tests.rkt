c4599afffd78a75e2139b7e05c83425f56fbff72
Nate Hofmann <inatejh@gmail.com>
2020-01-04 11:44:11 -0600

    More specific errors for valid problem, positive tests for valid problem

--- valid-problem-tests.rkt
+++ valid-problem-tests.rkt
@@ -6,37 +6,35 @@
 ; problem with no variables or no constraints
 (check-equal? (valid-problem? (problem (hash) (hash))) #t)
 
-; one variable, one constraint
+; one variable, one constraint, one constraint per variable
 (check-equal? 
  (valid-problem?
-  (make-problem '((a . (1))) (list (cons '(a) (list (lambda (x) (equal? x 1)))))))
+  (make-problem '((a . (1)))
+                (list (cons '(a) (list (λ (x) (equal? x 1)))))))
  #t)
 
-#|
-
-; one variable, mulitple constraints
+; one variable, mulitple constraints, one constraint per variable
 (check-equal? 
  (valid-problem?
-  (make-problem '((a . (1))) '(((a) . ((lambda (x) (equal? x 1))))
-                               ((a) . ((lambda (x) (not (equal? x 2)))))
-                               ((a) . ((lambda (x) (not (equal? x 3))))))))
+  (make-problem '((a . (1)))
+                (list
+                 (cons '(a) (list (λ (x) (equal? x 1))))
+                 (cons '(a)  (list (λ (x) (not (equal? x 2)))))
+                 (cons '(a)  (list (λ (x) (not (equal? x 3))))))))
  #t)
 
-; multiple variables, one constraint
-(check-equal? 
- (valid-problem?
-  (make-problem '((a . (1 2)) (b . (2 3)))
-                '(((a b) . ((lambda (x y) (equal? x y)))))))
- #t)
-
-; multiple variables, multiple constraints
+; multiple variables, one constraint, one constraint per variable
 (check-equal? 
  (valid-problem?
   (make-problem '((a . (1 2)) (b . (2 3)))
-                '(((a b) . ((lambda (x y) (equal? x y))))
-                  ((b) . ((lambda (y) (not (equal? y 3)))))
-                  ((a) . ((lambda (x) (not (equal? x 9))))))))
+                (list (cons '(a b) (list (λ (x y) (equal? x y)))))))
  #t)
 
-|#
-;;;
+; multiple variables, multiple constraints, one constraint per variable
+(check-equal? 
+ (valid-problem?
+  (make-problem '((a . (1 2)) (b . (2 3)))
+                (list (cons '(a b) (list (λ (x y) (equal? x y))))
+                      (cons '(b) (list (λ (x) (not (equal? x 3)))))
+                      (cons '(a) (list (λ (x) (not (equal? x 9))))))))
+ #t)
--- valid-problem.rkt
+++ valid-problem.rkt
@@ -4,26 +4,39 @@
 (require racket/local)
 (require "types.rkt")
 
+; Returns if every item in the given list returns true for the given function.
+; Function must take in one argument and return a boolean.
+; func : Any -> Boolean
+; items : Listof Any
+(define (all-func? func items)
+  (all (map func items)))
+
 ; Returns if the given list of booleans is all true.
 ; bools : Listof Boolean
 ; Return : Boolean  
 (define (all bools)
-  (cond
-    [(empty? bools) #t]
-    [(equal? (first bools) #f) #f]
-    [else (all (rest bools))]))
+  (foldr (λ (x y) (and x y)) #t bools))
 
-(define (all-func? func items)
-  (all (map func items)))
-
-; Returns if given list of items consists of all symbols.
-; items : List of items
+; Returns if given list of items consists of all symbols, else throws an error.
+; items : Listof Any
 ; Returns : Boolean
 (define (all-symbols? items)
-  (all-func? symbol? items))
+  (if (all-func? symbol? items)
+      #t
+      (raise-argument-error 'not-all-symbols
+                            "not all items in given list are symbols"
+                            items)))
 
+; Returns if given list of items consists of all procedures, else throws an
+; error.
+; items : Listof Any
+; Returns : Boolean
 (define (all-procedures? items)
-  (all-func? procedure? items))
+  (if (all-func? procedure? items)
+      #t
+      (raise-argument-error 'not-all-procedures
+                            "not all items in given list are procedures"
+                            items)))
 
 ; Returns if the variable attribute of the given problem is valid, i.e. does it
 ; consist of a mapping of symbols to list of integers.
@@ -31,11 +44,15 @@
 ; Return : Boolean
 (define (valid-variables-attribute? problem)
   (local (; Checks that the domain of the given variable only consists of
-          ; integers.
+          ; integers, else throws an error.
           ; domain : List of Integer
           ; Return : Boolean
           (define (variable-domain-integers? domain)
-            (all (map integer? domain)))
+            (if (all (map integer? domain))
+                #t
+                (raise-argument-error 'non-integer-variable-domain
+                                      "variable domain contains non integers"
+                                      domain)))
 
           (define (valid-domains? domains)
             (all (map variable-domain-integers? domains))))
@@ -48,8 +65,7 @@
 ; Return : boolean
 (define (valid-constraints-attribute? problem)
   (and (all (map all-symbols? (hash-keys (problem-constraints problem))))
-       (all (map all-procedures? (hash-values (problem-constraints problem))))
-       ))
+       (all (map all-procedures? (hash-values (problem-constraints problem))))))
 
 ; Checks that for each constraint of the given problem, number of variables
 ; assigned to that constraint match the number of arguments expected by its
@@ -71,17 +87,17 @@
             (if (empty? constraints)
                 #t
                 (let* ([cur-constraint (first constraints)]
-                   [num-of-vars (length (first cur-constraint))]
-                   [constraints (rest cur-constraint)])
-              (cond
-                [(all (map (lambda (x) (verify-constraint num-of-vars x))
-                           constraints))
-                 (verify-constraints-helper (rest constraints))]
-                [else (raise-argument-error 'mismatching-constraint-arity
-                                            ""
-                                            cur-constraint)])))))
-          (verify-constraints-helper
-           (hash->list (problem-constraints problem)))))
+                       [num-of-vars (length (first cur-constraint))]
+                       [constraints (rest cur-constraint)])
+                  (cond
+                    [(all (map (λ (x) (verify-constraint num-of-vars x))
+                               constraints))
+                     (verify-constraints-helper (rest constraints))]
+                    [else (raise-argument-error 'mismatching-constraint-arity
+                                                ""
+                                                cur-constraint)])))))
+    (verify-constraints-helper
+     (hash->list (problem-constraints problem)))))
 
 ; Checks that for the given problem, the variables assigned in the constraints
 ; of this problem do not mention a variable not assigned to the problem.
@@ -96,7 +112,7 @@
           (apply append
                  (hash-keys (problem-constraints problem)))]
          [unassigned-constraint-vars
-          (filter (lambda (x) (not (set-member? assigned-variables x)))
+          (filter (λ (x) (not (set-member? assigned-variables x)))
                   constraint-variables)])
     (if (empty? unassigned-constraint-vars)
         #t
@@ -105,21 +121,40 @@
          "constraints have variable(s) not assigned to problem"
          unassigned-constraint-vars))))
 
-; Returns if the given list of items has no duplicate items, i.e. is it a set.
-; items : List of T
-; Return : boolean
-(define (unique-list? items)
-  (equal? (length items) (length (set->list (list->set items)))))
-
 ; For the given problem, for each associated constraint, returns if variables
 ; are unique.
 ; problem : Problem
 ; Return : boolean
 (define (unique-constraint-variables? problem)
-  (all (map unique-list? (hash-keys (problem-constraints problem)))))
+  (local (; Returns if the given list of items has no duplicate items, i.e. is
+          ; it a set, else throws an error.
+          ; items : List of T
+          ; Return : boolean
+          (define (unique-list? items)
+            (if (equal? (length items) (length (set->list (list->set items))))
+                #t
+                (raise-argument-error 'duplicate-items
+                                      "duplicate items in the given list"
+                                      (get-dups (sort items <) '()))))
+
+          ; Returns all items that appear more than once in the given list.
+          ; List must be sorted.
+          ; items : Listof T
+          ; dups : Listof T
+          ; Return : Listof T
+          (define (get-dups items dups)
+            (cond
+              [(empty? items) dups]
+              [(equal? (length items) 1) dups]
+              [(and (equal? (first items) (second items))
+                    (or (empty? dups)
+                        (not (equal? (first items) (first dups)))))
+               (get-dups (rest (rest items)) (cons (first items) dups))]
+              [else (get-dups (rest items) dups)])))      
+    (all (map unique-list? (hash-keys (problem-constraints problem))))))       
 
 ; Verifies that the given problem is valid, i.e. that is meets the following
-; criteria:
+; criteria, a specific error should be thrown if a criteria isn't met:
 ; - variables attibute is mapping of symbols to domains
 ; - constraints attribute is mapping of a list of integers to a procedure
 ; - variables assigned to given constraints do not contain a variable not
@@ -136,5 +171,4 @@
        (valid-constraints-attribute? problem)
        (matching-constraints-arity? problem)
        (variable-overlap? problem)
-       (unique-constraint-variables? problem)
-       ))
+       (unique-constraint-variables? problem)))
